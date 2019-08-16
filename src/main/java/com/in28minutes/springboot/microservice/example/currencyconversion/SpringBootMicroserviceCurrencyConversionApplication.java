9d4f4f448d78e4006db395c9b03296d4ef896c3e
Abhinav Sinha <sinha.abhinav1991@gmail.com>
2019-08-16 15:46:36 +0530

    Error handling for non existent currency code.

--- src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
+++ src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.microservice.example.currencyconversion;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +10,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients("com.in28minutes.springboot.microservice.example.currencyconversion")
 /*@EnableDiscoveryClient*/
 public class SpringBootMicroserviceCurrencyConversionApplication {
+
+	@PostConstruct
+	public void init()
+	{
+		// Setting Spring Boot TimeZone
+		TimeZone.setDefault( TimeZone.getTimeZone( "IST" ) );
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
